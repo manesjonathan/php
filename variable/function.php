069e50a222a07c48d0e4279941e0a309a2cfec31
Jonathan Manes <manesjona@gmail.com>
2023-01-12 10:00:58 +0100

    function end

--- variable/function.php
+++ variable/function.php
@@ -82,4 +82,39 @@ echo feedback("Incorrect email address", "error");
 echo feedback("This is a warning", "warning");
 echo feedback("This is an info", "info");
 
-?>
+
+function random_word($minLength, $maxLength)
+{
+
+    $length = mt_rand($minLength, $maxLength);
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return '<p>' . ucfirst($randomString) . '</p>';
+}
+
+echo random_word(1, 5);
+echo random_word(7, 15);
+
+function de_capitalize($string)
+{
+    return '<p>' . ucfirst(strtolower($string)) . '</p>';
+
+}
+
+echo de_capitalize("STOP YELLING I CAN'T HEAR MYSELF THINKING!!");
+
+function calculate_cone_volume($ray, $height)
+{
+    $volume = round($ray * $ray * pi() * $height * (1 / 3), 2);
+
+    $message = 'The volume of a cone which ray is ' . $ray . 'cm and height is ' . $height . 'cm = ' . $volume . 'cm<sup>3</sup><br/>';
+
+    return $message;
+}
+
+echo calculate_cone_volume(5, 2);
+echo calculate_cone_volume(3, 4);
